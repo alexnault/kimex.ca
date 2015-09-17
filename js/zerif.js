8ac0f50c2235c53f46bcdb459227f2029d1c3a6f
Alex Nault <nault.alex@gmail.com>
2015-09-16 23:47:29 -0400

    Collapse navbar on click

--- js/zerif.js
+++ js/zerif.js
@@ -249,6 +249,14 @@ $(".skill2").knob({
                 });
 });
 
+/* ================================
+===  NAVBAR COLAPSE            ====
+================================= */
+$(document).on('click','.navbar-collapse.in',function(e) {
+  if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+    $(this).collapse('hide');
+  }
+});
 
 
 /* =================================
