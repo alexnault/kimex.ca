6651d19f5fb5e1b10a12c2382f95a3601f1914aa
Alex Nault <nault.alex@gmail.com>
2015-09-24 13:28:56 -0400

    Remove single-project page

--- js/zerif.js
+++ js/zerif.js
@@ -152,48 +152,6 @@ $("#contact-form").submit(function (e) {
     return false;
 });
 
-
-/* ================================
-===  PROJECT LOADING           ====
-================================= */
-
-// jQuery(document).ready(function($) {
-//     $('.more').on('click', function(event) {
-//         event.preventDefault();
-
-//         var href = $(this).attr('href') + ' .single-project',
-//             portfolioList = $('#portfolio-list'),
-//             content = $('#loaded-content');
-
-//         portfolioList.animate({'marginLeft':'-120%'},{duration:400,queue:false});
-//         portfolioList.fadeOut(400);
-//         setTimeout(function(){ $('#loader').show(); },400);
-//         setTimeout(function(){
-//             content.load(href, function() {
-//                 $('#loaded-content meta').remove();
-//                 $('#loader').hide();
-//                 content.fadeIn(600);
-//                 $('#back-button').fadeIn(600);
-//             });
-//         },800);
-
-//     });
-
-//     $('#back-button').on('click', function(event) {
-//         event.preventDefault();
-
-//         var portfolioList = $('#portfolio-list')
-//             content = $('#loaded-content');
-
-//         content.fadeOut(400);
-//         $('#back-button').fadeOut(400);
-//         setTimeout(function(){
-//             portfolioList.animate({'marginLeft':'0'},{duration:400,queue:false});
-//             portfolioList.fadeIn(600);
-//         },800);
-//     });
-// });
-
 /* ================================
 ===  PARALLAX                  ====
 ================================= */
